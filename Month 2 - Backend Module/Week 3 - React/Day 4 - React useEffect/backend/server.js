ed912f51bd1b1a186fd83da937d9e74c9b8ed6e6
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2024-11-21 12:32:11 -0500

    Delete Month 2 - Backend Module directory

--- Month 2 - Backend Module/Week 3 - React/Day 4 - React useEffect/backend/server.js
+++
@@ -1,8 +0,0 @@
-const { db } = require("./config/connection.js");
-const app = require("./src/app.js");
-const port = 3000;
-
-app.listen(port, async () => {
-  await db.sync();
-  console.log(`Listening on port: ${port}`);
-});
